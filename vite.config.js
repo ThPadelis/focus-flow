4df08a3d35c654289e4762d3e38f8ba2a9bbd20b
Pantelis Theodosiou <padelis.theodosiou@gmail.com>
2025-11-30 14:39:27 +0200

    fix: update PWA configuration

--- vite.config.js
+++ vite.config.js
@@ -21,18 +21,18 @@ export default defineConfig({
         theme_color: '#09090b',
         background_color: '#09090b',
         display: 'standalone',
-        scope: '/',
-        start_url: '/',
+        scope: './',
+        start_url: './',
         orientation: 'portrait',
         icons: [
           {
-            src: '/icon-192.png',
+            src: 'icon-192.png',
             sizes: '192x192',
             type: 'image/png',
             purpose: 'any maskable'
           },
           {
-            src: '/icon-512.png',
+            src: 'icon-512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable'

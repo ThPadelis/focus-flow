aa905ac6718f0e456fc3ba677e02e27cdee783af
Pantelis Theodosiou <padelis.theodosiou@gmail.com>
2025-12-01 21:58:59 +0200

    feat: update favicons and manifest
    - Update index.html with new favicon links and meta tags
    - Add favicon-96x96.png to site.webmanifest
    - Update service worker assets

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ca84f546'], (function (workbox) { 'use strict';
     "revision": "d41d8cd98f00b204e9800998ecf8427e"
   }, {
     "url": "index.html",
-    "revision": "0.0o1odvq21ro"
+    "revision": "0.94r40v9b6b"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
